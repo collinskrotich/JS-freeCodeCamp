8b53461f0984ff05ef92f16000c493e66225806c
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-04 21:11:54 +0300

    default option for switch statements

--- switchstatements.js
+++ switchstatements.js
@@ -52,3 +52,40 @@ function caseInSwitch(val) {
   caseInSwitch(2);
   caseInSwitch(3);
   caseInSwitch(4);
+
+
+// Adding a Default Option in Switch Statements
+// In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
+
+// A default statement should be the last case.
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case "a":
+    answer = "apple";
+    break;
+
+    case "b":
+    answer = "bird";
+    break;
+
+    case "c":
+    answer = "cat";
+    break;
+
+    default:
+      answer = "stuff";
+      break;
+
+  }
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff("a");
+switchOfStuff("b");
+switchOfStuff("c");
+switchOfStuff("d");
+switchOfStuff(4);
