bf0f8b70fe6217b2b7ad4dc3b3da67c0e3fc3516
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-05 06:12:46 +0300

    return early pattern for functions

--- switchstatements.js
+++ switchstatements.js
@@ -121,3 +121,18 @@ But there's a better way to do this. Since === returns true or false, we can ret
 function isEqual(a, b) {
   return a === b;
 }
+
+// Return Early Pattern for Functions
+// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+
+// The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+
