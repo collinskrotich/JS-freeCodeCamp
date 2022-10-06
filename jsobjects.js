509e41f5041c6ec11bd9046e383480a793928866
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-06 20:22:50 +0300

    Thursday push

---
+++ jsobjects.js
@@ -0,0 +1,47 @@
+// Build JavaScript Objects
+// You may have heard the term object before.
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+// Here's a sample cat object:
+
+const cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+
+// In this example, all the properties are stored as strings, such as name, legs, and tails. However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
+
+const anotherObject = {
+  make: "Ford",
+  5: "five",
+  "model": "focus"
+};
+
+// However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+// Accessing Object Properties with Bracket Notation
+// The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+
+// However, you can still use bracket notation on object properties without spaces.
+
+// Here is a sample of using bracket notation to read an object's property:
+
+const myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+
+myObj["Space Name"];
+myObj['More Space'];
+myObj["NoSpace"];
+myObj["Space Name"] 
+
+// would be the string Kirk, myObj['More Space'] would be the string Spock, and myObj["NoSpace"] would be the string USS Enterprise.
+
+// Note that property names with spaces in them must be in quotes (single or double).
