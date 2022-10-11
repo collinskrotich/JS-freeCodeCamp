91c99ee9cd8da0d75b82c41d72b206cd56e3e514
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-11 17:03:38 +0300

    Countdown app

--- conditionaloperator.js
+++ conditionaloperator.js
@@ -56,4 +56,14 @@ It is considered best practice to format multiple conditional operators such tha
 function findGreaterOrEqual(a, b) {
   return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
 }
-In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+// In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. 
+// The function should return positive, negative or zero.
+
+function checkSign(num) {
+    return (num > 0) ? "positive":
+    (num < 0) ? "negative":
+    "zero";
+  
+  }
+  
+  checkSign(10);
